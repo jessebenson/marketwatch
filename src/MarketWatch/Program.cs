d8630aca30ec106dca6b16e1934a3cc083b63dbf
Jesse Benson <jesse.benson@microsoft.com>
2016-10-16 20:42:56 -0700

    Add Serilog and Elasticsearch logging.

--- src/MarketWatch/Program.cs
+++ src/MarketWatch/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System.Configuration;
 
 namespace MarketWatch
 {
@@ -10,6 +13,22 @@ namespace MarketWatch
     {
         static void Main(string[] args)
         {
+            var elasticsearchUri = new Uri(ConfigurationManager.AppSettings["ElasticsearchUri"]);
+            var elasticsearchUsername = ConfigurationManager.AppSettings["ElasticsearchUsername"];
+            var elasticsearchPassword = ConfigurationManager.AppSettings["ElasticsearchPassword"];
+
+            var options = new ElasticsearchSinkOptions(elasticsearchUri)
+            {
+                ModifyConnectionSettings = c => c.BasicAuthentication(elasticsearchUsername, elasticsearchPassword),
+            };
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(options)
+                .CreateLogger();
+
+            Console.WriteLine("Press enter to exit ...");
+            Console.ReadLine();
         }
     }
 }
